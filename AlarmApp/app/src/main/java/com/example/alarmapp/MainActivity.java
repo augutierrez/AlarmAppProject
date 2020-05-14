457fe9edf71adab5b9f59922841d3b1d1665d3c2
augutierrez <augutierrez@dons.usfca.edu>
2020-05-14 16:54:36 -0700

    first commit

---
+++ AlarmApp/app/src/main/java/com/example/alarmapp/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.alarmapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    String TAG = "MainActivity";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //adds click functionality to the new alarm button
+        Button add_alarm_button = (Button) findViewById(R.id.add_alarm);
+        add_alarm_button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.add_alarm: {
+                Toast.makeText(this, "Creating Alarm", Toast.LENGTH_LONG).show();
+                Log.i(TAG, "add alarm button was clicked");
+                Intent intent = new Intent(MainActivity.this, ActivityTwo.class);
+                startActivity(intent);
+                break;
+            }
+            default:
+                Toast.makeText(getApplicationContext(), "What Happened?", Toast.LENGTH_LONG).show();
+                Log.i(TAG, "default case was reached");
+        }
+    }
+}
